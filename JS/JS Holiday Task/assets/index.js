ab1c8e2ce9fd39b5dd02ff2a1baeb3ca1528ccc0
NijatMMammadov <nicatmm-ap214@code.edu.az>
2024-11-13 19:56:56 +0400

    task

--- JS/JS Holiday Task/assets/index.js
+++ JS/JS Holiday Task/assets/index.js
@@ -4,20 +4,30 @@ let myArray = [
         "Name": "Mark ,",
         "age": 12 + "years old",
     },
+    {
+        "image": "./assets/images/children_4.jpg.webp",
+        "Name": "Andre ,",
+        "age": 10 + "years old",
+    },
+    {
+        "image": "./assets/images/children_5.jpg.webp",
+        "Name": "Markau ,",
+        "age": 11 + "years old",
+    }
 ]
 
-let navbar = document.querySelector("navbar");
-let childrenBoxes = document.querySelector("children-boxes");
-let box = document.querySelector("box");
+let navbar = document.querySelector(".navbar");
+let childrenBoxes = document.querySelector(".children-boxes");
+let box = document.querySelector(".box");
 
 
-document.addEventListener("DOMContentLoaded",Show(myArray))
+document.addEventListener("DOMContentLoaded", Show(myArray))
 
 
-function Show(myArray) {
+function Show() {
     myArray.forEach(myArray => {
-        childrenBoxes.innerHTML`
-        <div class="box">
+        childrenBoxes.innerHTML += `
+                    <div class="box">
                         <div class="box-image">
                             <img src=${myArray.image} alt="">
                         </div>
@@ -26,26 +36,39 @@ function Show(myArray) {
                             <p class="p-age"> ${myArray.age}</p>
                         </div>
                         <div class="button-dd">
-                            <button class="btn-details">Detils</button>
+                            <button class="btn-details">Details</button>
                             <button class="btn-delete">Delete</button>
                         </div>
                     </div> `
     })
+
 }
 
-Show(myArray)
+// Show(myArray)
 
 //          ``
 
 window.addEventListener("scroll", () => {
     let scrollPosition = window.scrollY
-    let navbar = document.querySelector("navbar");
+    let navbar = document.querySelector(".navbar");
     if (scrollPosition > 50) {
         console.log("salam");
-        navbar.style.backgroundColor = "red"
-        navbar.style.color = "white"
+        navbar.style.backgroundColor = "rgb(188, 81,72)";
+        navbar.style.color = "white";
     } else {
-        navbar.style.backgroundColor = "white"
-        navbar.style.color = "black"
+        navbar.style.backgroundColor = "white";
+        navbar.style.color = "black";
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
