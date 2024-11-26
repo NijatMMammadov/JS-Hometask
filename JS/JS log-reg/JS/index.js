02b7cb10187975eff2e08c6066214c298f6706ac
zaur <zaurrs-af106@code.edu.az>
2024-11-26 19:34:59 +0400

    noybr

--- JS/JS log-reg/JS/index.js
+++ JS/JS log-reg/JS/index.js
@@ -7,12 +7,6 @@ let row=document.querySelector(".row");
 
 
 
-btnDxl.addEventListener("click",()=>{
-    window.location.href = "login.html"
-    console.log("sss");
-    
-})
-
 function GetProducts(){
     GetAllData(`${BaseURL}/products`)
     .then(res=>ShowProducts(res.data))
@@ -23,20 +17,17 @@ GetProducts()
 function ShowProducts(products){
     products.forEach((product) => {
         row.innerHTML+=`
-         <div class="col my-3">
-        <div class="card" style="width: 18rem;">
-          <img src=${product.image} class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text">${product.price}$</p>
-            <div class="btns">
-            <a href="#" data-id=${product.id} class="btn fav btn-outline-primary"><i class="fa-solid fa-heart"></i></a>
-            <a href="#" class="btn btn-outline-danger"><i class="fa-solid fa-basket-shopping"></i> Səbətə at</a>
+          <div class="card my-3 mx-4" style="width: 18rem;">
+            <img src="${product.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-brend">${product.brand}</h5>
+              <h4 class="card-model">${product.model}</h4>
+              <p class="card-sys">${product.operatingSystem}</p>
+              <p class="card-sys">${product.year}</p>
+              <p class="card-text">${product.price}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
-        </div>
-      </div>
         `
 
 
@@ -49,6 +40,11 @@ function ShowProducts(products){
 }
 
 
+
+
+btnDxl.addEventListener("click",()=>{
+  window.location.href = "login.html"
+})
 
 
 
--- JS/JS log-reg/JS/register.js
+++ JS/JS log-reg/JS/register.js
@@ -1,20 +1,33 @@
+import BaseURL from "./Api/BaseURL.js";
+import { PostData } from "./Api/request/requests.js";
 
+
+let resgisterForm= document.querySelector(".resgister-form");
 let regName = document.querySelector("#reg-name");
 let regSurname = document.querySelector("#reg-surname");
 let regEmail = document.querySelector("#reg-email");
 let regPassword = document.querySelector("#reg-password");
 
 
-let newdata = {
-    name: regName.value,
-    surname:regSurname.value,
-    email:regEmail.value,
-    password:regPassword.value,
-    isAdmin:false,
-    favorites:[]
-}
+resgisterForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let postdata = {
+        name:regName.value,
+        surname:regSurname.value,
+        email:regEmail.value,
+        password:regPassword.value,
+        isAdmin:false,
+        favorites:[]
+    }
+    
+    console.log(postdata);
+    
+    PostData(`${BaseURL}/users`,postdata)
 
-console.log(newdata);
+})
+
+
+
 
 
 
